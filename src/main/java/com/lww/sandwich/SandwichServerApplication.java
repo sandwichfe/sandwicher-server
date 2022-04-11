3299253edab6636e5985cb12a8cf2bdd108c50a2
sandwichfe <1344382941@qq.com>
2022-04-11 17:24:04 +0800

    更新稳定版本swagger及Knife4j

--- src/main/java/com/lww/sandwich/SandwichServerApplication.java
+++ src/main/java/com/lww/sandwich/SandwichServerApplication.java
@@ -4,15 +4,12 @@ import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import springfox.documentation.oas.annotations.EnableOpenApi;
 
 /**
  * @author lww
  * @Date 2022-3-7
  */
 @SpringBootApplication
-@EnableOpenApi
-@EnableKnife4j
 @MapperScan("com.lww.sandwich.mapper")
 public class SandwichServerApplication {
     public static void main(String[] args) {
