7005949f2c498658d97c100951b355fc8ab4674a
lww <1344382941@qq.com>
2024-11-26 14:21:56 +0800

    注释

--- sandwich-parent/sandwich-auth/auth-server/src/main/java/com/lww/auth/server/OauthServerApplication.java
+++ sandwich-parent/sandwich-auth/auth-server/src/main/java/com/lww/auth/server/OauthServerApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 /**
  * Hello world!
  *
+ * @author lww
  */
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class OauthServerApplication {
