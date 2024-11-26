44621de7d7917511d04aeb46336663ff9d2c0fd9
lww <1344382941@qq.com>
2024-11-26 18:11:08 +0800

    更换为代码配置 替代配置文件

--- sandwich-parent/sandwich-auth/auth-server/src/main/java/com/lww/auth/server/config/SecurityConfiguration.java
+++ sandwich-parent/sandwich-auth/auth-server/src/main/java/com/lww/auth/server/config/SecurityConfiguration.java
@@ -1,20 +1,54 @@
 package com.lww.auth.server.config;
 
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.core.oidc.OidcScopes;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
+import org.springframework.security.oauth2.server.authorization.settings.AuthorizationServerSettings;
+import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import java.util.UUID;
 
 @Configuration
+@EnableWebSecurity
 class SecurityConfiguration {
 
-    // OAuth2AuthorizationServer配置
+    /**
+     * OAuth2AuthorizationServer配置
+     *
+     * @author lww
+     * @since 2024/11/26
+     */
     @Bean
-    public SecurityFilterChain serverFilterChain(HttpSecurity http) throws Exception {
+    @Order(1)
+    public SecurityFilterChain authorizationServerSecurityFilterChain(HttpSecurity http) throws Exception {
         //授权服务器的安全交给security的过滤器处理
         OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http);
         // 自定义配置
@@ -28,15 +62,116 @@ class SecurityConfiguration {
         return http.build();
     }
 
-    // security配置
+    /**
+     * security配置
+     *
+     * @author lww
+     * @since 2024/11/26
+     */
     @Bean
+    @Order(2)
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated());
+        http.authorizeHttpRequests(authorize -> authorize.requestMatchers(
+                                // 不拦截
+                                new AntPathRequestMatcher("/auth/**"),
+                                new AntPathRequestMatcher("/oauth2/**"))
+                        .permitAll()
+                // 其他请求需要认证
+                .anyRequest().authenticated());
         http.formLogin(form -> form
                 // 自定义登录页面
                 .loginPage("/login")
                 // 处理登录请求接口
                 .loginProcessingUrl("/login").permitAll());
+        // 接受用户信息和/或客户端注册的访问令牌
+        http.oauth2ResourceServer(resourceServer -> resourceServer
+                .jwt(Customizer.withDefaults()));
         return http.build();
     }
+
+    /**
+     * springSecurity 的用户
+     *
+     * @author lww
+     * @since 2024/11/26
+     */
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails userDetails = User.withDefaultPasswordEncoder()
+                .username("lww")
+                .password("123456")
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(userDetails);
+    }
+
+    /**
+     * 客户端应用注册
+     *
+     * @author lww
+     * @since 2024/11/26
+     */
+    @Bean
+    public RegisteredClientRepository registeredClientRepository() {
+        RegisteredClient oidcClient = RegisteredClient.withId(UUID.randomUUID().toString())
+                // 客户端名称 myClient
+                .clientName("myClient")
+                // 客户端id
+                .clientId("client_lww")
+                .clientSecret("{noop}123456")
+                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+                // 授权类型 授权码 refresh_token
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
+                // 客户端回调地址
+                .redirectUri("http://localhost:8080/login/oauth2/code/myClient")
+                // logout后回调地址
+                .postLogoutRedirectUri("http://127.0.0.1:8080/")
+                // 授权范围
+                // 用户的唯一标识符
+                .scope(OidcScopes.OPENID)
+                // 用户的个人信息
+                .scope(OidcScopes.PROFILE)
+                // 是否需要用户授权确认
+                .clientSettings(ClientSettings.builder().requireAuthorizationConsent(true).build())
+                .build();
+
+        return new InMemoryRegisteredClientRepository(oidcClient);
+    }
+
+    @Bean
+    public JWKSource<SecurityContext> jwkSource() {
+        KeyPair keyPair = generateRsaKey();
+        RSAPublicKey publicKey = (RSAPublicKey) keyPair.getPublic();
+        RSAPrivateKey privateKey = (RSAPrivateKey) keyPair.getPrivate();
+        RSAKey rsaKey = new RSAKey.Builder(publicKey)
+                .privateKey(privateKey)
+                .keyID(UUID.randomUUID().toString())
+                .build();
+        JWKSet jwkSet = new JWKSet(rsaKey);
+        return new ImmutableJWKSet<>(jwkSet);
+    }
+
+    private static KeyPair generateRsaKey() {
+        KeyPair keyPair;
+        try {
+            KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+            keyPairGenerator.initialize(2048);
+            keyPair = keyPairGenerator.generateKeyPair();
+        } catch (Exception ex) {
+            throw new IllegalStateException(ex);
+        }
+        return keyPair;
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder(JWKSource<SecurityContext> jwkSource) {
+        return OAuth2AuthorizationServerConfiguration.jwtDecoder(jwkSource);
+    }
+
+    @Bean
+    public AuthorizationServerSettings authorizationServerSettings() {
+        return AuthorizationServerSettings.builder().build();
+    }
+
 }
