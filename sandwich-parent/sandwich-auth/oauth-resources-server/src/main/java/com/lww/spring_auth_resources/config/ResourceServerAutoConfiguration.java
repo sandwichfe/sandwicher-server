ecf70abeac944815f781e59022ea55c104d799b3
lww <1344382941@qq.com>
2024-11-28 17:40:54 +0800

    oauth-resource配置

--- sandwich-parent/sandwich-auth/oauth-resources-server/src/main/java/com/lww/spring_auth_resources/config/ResourceServerAutoConfiguration.java
+++ sandwich-parent/sandwich-auth/oauth-resources-server/src/main/java/com/lww/spring_auth_resources/config/ResourceServerAutoConfiguration.java
@@ -9,6 +9,7 @@ package com.lww.spring_auth_resources.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -16,10 +17,19 @@ import org.springframework.security.web.SecurityFilterChain;
 public class ResourceServerAutoConfiguration {
 
     @Bean
-    public SecurityFilterChain resourceServerSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests().anyRequest().authenticated().and()
-                .oauth2ResourceServer().jwt();
-
+    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(authorize -> authorize
+                        // 下边一行是放行接口的配置，被放行的接口上不能有权限注解，e.g. @PreAuthorize，否则无效
+                        // .requestMatchers("/test02").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2
+                                // 可在此处添加自定义解析设置
+                                .jwt(Customizer.withDefaults())
+                        // 添加未携带token和权限不足异常处理(已在第五篇文章中说过)
+//                        .accessDeniedHandler(SecurityUtils::exceptionHandler)
+//                        .authenticationEntryPoint(SecurityUtils::exceptionHandler)
+                );
         return http.build();
     }
 }
