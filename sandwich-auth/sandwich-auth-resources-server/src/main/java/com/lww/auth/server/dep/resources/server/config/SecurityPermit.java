ecc57827ce3adc3628cb862c7934d436e66afc97
lww <1344382941@qq.com>
2025-08-27 16:12:20 +0800

    模块梳理-ttt

---
+++ sandwich-auth/sandwich-auth-resources-server/src/main/java/com/lww/auth/server/dep/resources/server/config/SecurityPermit.java
@@ -0,0 +1,25 @@
+package com.lww.auth.server.dep.resources.server.config;
+
+/**
+ * 忽略权限校验接口
+ * @author lww
+ * @since 2024/12/9
+ */
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Getter
+@Setter
+@Component
+@ConfigurationProperties(prefix = "security.permit")
+public class SecurityPermit {
+
+    private List<String> urls = new ArrayList<>();
+
+}
